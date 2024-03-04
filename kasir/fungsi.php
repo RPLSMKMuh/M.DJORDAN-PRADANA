b079f592308b9202dd0dc1b32308be16f253e8ba
Djordannn <161820966+Djordannn@users.noreply.github.com>
2024-03-04 16:08:14 +0700

    Add files via upload

--- kasir/fungsi.php
+++ kasir/fungsi.php
@@ -9,9 +9,10 @@ function regist($data) {
     $email = $data['email'];
     $password = mysqli_real_escape_string($conn, $data['password']);
     $password2 = mysqli_real_escape_string($conn, $data['password2']);
+    $level = $data['level'];
     
     //cek username sudah terdaftar atau belum
-    $result = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+    $result = mysqli_query($conn, "SELECT username FROM petugas WHERE username = '$username'");
     if(mysqli_fetch_assoc($result)) {
         echo "<script>alert('Username sudah ada!!');</script>";
         return false;
@@ -26,7 +27,7 @@ function regist($data) {
     //enkripsi password
     // $password = password_hash($password, PASSWORD_DEFAULT);
 
-    $query = "INSERT INTO user VALUES('', '$username', '$email', '$password')";
+    $query = "INSERT INTO petugas VALUES('', '$username', '$email', '$password', '$level')";
     mysqli_query($conn, $query);
 
     return mysqli_affected_rows($conn);
@@ -80,11 +81,12 @@ function tambahProduk($data) {
     $namaProduk = $data['nama'];
     $harga = $data['harga'];
     $stok = $data['stok'];
-    $foto = $data['foto'];
-    // $foto = upload();
-    // if(!$foto) {
-    //     return false;
-    // }
+    $foto = $_FILES['foto']['name'];
+
+    $dir = '../img/';
+    $tmpFile = $_FILES['foto']['tmp_name'];
+
+    move_uploaded_file($tmpFile, $dir.$foto);
 
     $query = "INSERT INTO produk VALUES('', '$namaProduk', '$harga', '$stok', '$foto')";
     mysqli_query($conn, $query);
@@ -92,39 +94,39 @@ function tambahProduk($data) {
     return mysqli_affected_rows($conn);
 }
 
-function upload() {
-    $nama = $_FILES['foto']['name'];
-    $size = $_FILES['foto']['size'];
-    $eror = $_FILES['foto']['error'];
-    $tmp  = $_FILES['foto']['tmp_name'];
+// function upload() {
+//     $nama = $_FILES['foto']['name'];
+//     $size = $_FILES['foto']['size'];
+//     $eror = $_FILES['foto']['error'];
+//     $tmp  = $_FILES['foto']['tmp_name'];
 
-    if($eror) {
-        echo "<script>alert('Foto anda kosong!!');</script>";
-        return false;
-    }
+//     if($eror) {
+//         echo "<script>alert('Foto anda kosong!!');</script>";
+//         return false;
+//     }
 
-    $ekstensifotovalid = ['jpg', 'jpeg', 'png'];
-    $ekstensifoto = explode('.', $nama);
-    $ekstensifoto = strtolower(end($ekstensifoto));
-    if(!in_array($ekstensifoto, $ekstensifotovalid)) {
-        echo "<script>alert('Format foto harus jpg, jpeg, png!!');</script>";
-        return false;
-    }
+//     $ekstensifotovalid = ['jpg', 'jpeg', 'png'];
+//     $ekstensifoto = explode('.', $nama);
+//     $ekstensifoto = strtolower(end($ekstensifoto));
+//     if(!in_array($ekstensifoto, $ekstensifotovalid)) {
+//         echo "<script>alert('Format foto harus jpg, jpeg, png!!');</script>";
+//         return false;
+//     }
 
-    if($size > 2000000) {
-        echo "<script>alert('Ukuran foto harus dibawah 2mb!!');</script>";
-        return false;
-    }
+//     if($size > 2000000) {
+//         echo "<script>alert('Ukuran foto harus dibawah 2mb!!');</script>";
+//         return false;
+//     }
 
-    $namabaru = uniqid();
-    $namabaru .= '.';
-    $namabaru .= $ekstensifoto;
+//     $namabaru = uniqid();
+//     $namabaru .= '.';
+//     $namabaru .= $ekstensifoto;
 
-    move_uploaded_file($tmp, '../img/'.$namabaru);
+//     move_uploaded_file($tmp, '../img/'.$namabaru);
 
-    return $nama;
+//     return $nama;
     
-}
+// }
 
 function editProduk($data) {
     global $conn;
@@ -133,7 +135,12 @@ function editProduk($data) {
     $namaProduk = $data['nama'];
     $harga = $data['harga'];
     $stok = $data['stok'];
-    $foto = $data['foto'];
+    $foto = $_FILES['foto']['name'];
+
+    $dir = '../img/';
+    $tmpFile = $_FILES['foto']['tmp_name'];
+
+    move_uploaded_file($tmpFile, $dir.$foto);
 
     $query = "UPDATE produk SET namaProduk='$namaProduk', harga='$harga', stok='$stok', foto='$foto' WHERE idProduk = $id";
     mysqli_query($conn, $query);
@@ -153,7 +160,7 @@ function editProfile($data) {
 
     $query = "UPDATE pelanggan SET nama='$nama', username='$username', password='$password', alamat='$alamat', notelepon='$notelp' WHERE idPelanggan = $id";
     mysqli_query($conn, $query);
-    
+
     return mysqli_affected_rows($conn);
     
 }
@@ -177,19 +184,42 @@ function editPelanggan($data) {
 function beli($data) {
     global $conn;
 
-    // $id = $_GET['id'];
-    // $produk = $data['produk'];
+    // Ambil data dari $data
     $namaProduk = $data['nama'];
     $tgl = $data['tanggal'];
     $harga = $data['harga'];
     $jumlah = $data['jumlah'];
     $idPelanggan = $data['pelanggan'];
-    $totalHarga = $harga * $jumlah; // Hitung total harga
+    $totalHarga = $harga * $jumlah;
 
-    // Masukkan data penjualan ke dalam tabel penjualan
-    $query = "INSERT INTO penjualan VALUES('', '$tgl', '$namaProduk', '$harga', '$jumlah', '$totalHarga' ,'$idPelanggan')";
-    mysqli_query($conn, $query);
 
-    return mysqli_affected_rows($conn);
+    $queryCekStok = "SELECT stok FROM produk WHERE namaProduk = '$namaProduk'";
+    $result = mysqli_query($conn, $queryCekStok);
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        $stok = $row['stok'];
+        if ($stok < $jumlah) {
+            echo "Stok habis";
+            return "Stok produk $namaProduk tidak mencukupi untuk pembelian ini!";
+        }
+    } else {
+        return "Terjadi kesalahan saat memeriksa stok produk!";
+    }
+
+    $query = "INSERT INTO penjualan VALUES ('', '$tgl', '$namaProduk', $harga, $jumlah, $totalHarga, $idPelanggan)";
+    
+    if (mysqli_query($conn, $query)) {
+        $queryUpdateStok = "UPDATE produk SET stok = stok - $jumlah WHERE namaProduk = '$namaProduk'";
+        mysqli_query($conn, $queryUpdateStok);
+
+        echo "<script>alert('Pembelian produk $namaProduk berhasil!');</script>";
+        echo "<script>window.location.href='riwayat.php';</script>";
+        return "Pembelian produk $namaProduk berhasil!";
+    } else {
+
+        return "Terjadi kesalahan saat melakukan pembelian!";
+    }
 }
+
+
 ?>
